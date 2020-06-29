b1944e8be2c2fa9ee5bca0983a900fd52dad318d
John Herman <hermanjohn2@gmail.com>
2020-06-29 13:03:36 -0400

    Added code for Heroku deployment

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -4,61 +4,64 @@ let db;
 const request = indexedDB.open('budget', 1);
 
 // If db hasn't been created or updated, create 'pending' obj store
-request.onupgradeneeded = (e) => {
-    db = e.target.result;
-    db.createObjectStore('pending', { autoIncrement: true });
+request.onupgradeneeded = e => {
+	db = e.target.result;
+	db.createObjectStore('pending', { autoIncrement: true });
 };
 
 // Upon success of opening the budget db, check if user is online and if they are, check for stored 'pending' obj in indexedDB
-request.onsuccess = (e) => {
-    db = event.target.result;
+request.onsuccess = e => {
+	db = event.target.result;
 
-    if (navigator.onLine) checkDatabase();
+	if (navigator.onLine) checkDatabase();
 };
 
-request.onerror = (e) => {
-    console.log(`Error: ${e.target.errorCode}`);
+request.onerror = e => {
+	console.log(`Error: ${e.target.errorCode}`);
 };
 
 // Helper Function for failed fetch when posting to our Mongo DB -- used in catch error handler
 function saveRecord(record) {
-    console.log('Saving record');
-    const transaction = db.transaction(['pending'], 'readwrite');
-    const store = transaction.objectStore('pending');
+	console.log('Saving record');
+	const transaction = db.transaction(['pending'], 'readwrite');
+	const store = transaction.objectStore('pending');
 
-    store.add(record);
+	store.add(record);
 }
 
 // Checking our indexed DB for any pending post's. If there is any pending, they are posted to our Mongo DB
 function checkDatabase() {
-    console.log('checking DB');
-    const transaction = db.transaction(['pending'], 'readwrite');
-    const store = transaction.objectStore('pending');
-    const getAll = store.getAll();
+	console.log('checking DB');
+	const transaction = db.transaction(['pending'], 'readwrite');
+	const store = transaction.objectStore('pending');
+	const getAll = store.getAll();
 
-    getAll.onsuccess = () => {
-        if (getAll.result.length > 0) {
-            fetch('/api/transaction/bulk', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then((res) => res.json())
-                .then(() => {
-                    const transaction = db.transaction(
-                        ['pending'],
-                        'readwrite'
-                    );
+	getAll.onsuccess = () => {
+		if (getAll.result.length > 0) {
+			fetch('/api/transaction/bulk', {
+				method: 'POST',
+				body: JSON.stringify(getAll.result),
+				headers: {
+					Accept:
+						'application/json, text/plain, */*',
+					'Content-Type': 'application/json'
+				}
+			})
+				.then(res => res.json())
+				.then(() => {
+					const transaction = db.transaction(
+						['pending'],
+						'readwrite'
+					);
 
-                    const store = transaction.objectStore('pending');
+					const store = transaction.objectStore(
+						'pending'
+					);
 
-                    store.clear();
-                });
-        }
-    };
+					store.clear();
+				});
+		}
+	};
 }
 
 window.addEventListener('online', checkDatabase);
